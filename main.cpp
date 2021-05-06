5369b2633dad72ec2ecba171c6b2f4b0e55e0ecf
DajanekDavis <>
2021-05-06 01:29:48 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+// Dajanek Davis
+// Tower of Hanoi
+// May 4, 2021 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int factorialR(int n)
+{
+   if(n<=1){
+      return n;
+   }
+   return n*factorialR(n-1);
+}
+ 
+int main()
+{
+  // Variables
+   int n;
+
+   cout << "Please enter a number for the starting point: ";
+
+   cin>>n;//Prompting user for input
+
+   cout << "The factorial is: ";
+
+   cout<<factorialR(n)<<endl;//Recursive call
+   return 0;
+}
